48743bc50e5889193a9b10167d10bf60e3c7d072
NoorulAmeenJ <noorularafat786@gmail.com>
2022-12-30 20:31:26 +0530

    gender completed

--- script.js
+++ script.js
@@ -1,54 +1,51 @@
 var finput =document.getElementById("first-input")
 var linput =document.getElementById("last-input")
-var submit =document.getElementById("submit")
 var addinput= document.getElementById("address-text") 
 var pininput = document.getElementById("input-pincode")
-// var gendername = document.getElementsByName("gender")
+var gendername = document.getElementsByName("gender")
 var parotainput = document.getElementById(" input-Parota")
 var stateinput = document.getElementById("input-state")
 var countryinput = document.getElementById("input-country")
-
-
+// console.log(gendername)
+var submit =document.getElementById("submit")
 
 var count= 0
 submit.addEventListener("click", (e)=>{
     e.preventDefault();
-// addinput.value= `${finput.value} ${linput.value} ${pininput.value} ${linput.value} ${gendername.value} ${stateinput.value} ${countryinput.value}`
-
-var ele = document.getElementsByName('food');
-              
-for(i = 0; i < ele.length; i++) {
-    // console.log(ele[i].value)
-    if(ele[i].checked){
-        count++
-    addinput.value= "food: "+ele[i].value;
-    console.log(count)
-}
+// for gender radio boxes
+var gendername = document.getElementsByName('gender');    
+for(i=0; i<gendername.length; i++) 
+{
+    if(gendername[i].checked == true){
+      var genderin = gendername[i].value;
     }
-   
-count=0
+}            
 var tablebody = document.getElementById("table-body")
-var asd = tablebody.insertRow(-1);
+var newrow = tablebody.insertRow(-1);
 
-var dur =asd.insertCell(-1);
-dur.innerText = finput.value
- var dar = asd.insertCell(-1);
-dar.innerText = "sinput.value"
+var cell1 =newrow.insertCell(-1);
+cell1.innerText = finput.value
 
+ var cell2 = newrow.insertCell(-1);
+cell2.innerText = linput.value
+
+var cell3 = newrow.insertCell(-1);
+cell3.innerText = addinput.value
+
+var cell4 = newrow.insertCell(-1);
+cell4.innerText = pininput.value
+
+var cell5 = newrow.insertCell(-1);
+cell5.innerText = genderin
+
+var cell6 = newrow.insertCell(-1);
+cell6.innerText = genderin
+
+var cell7 = newrow.insertCell(-1);
+cell7.innerText = stateinput.value
+
+var cell8 = newrow.insertCell(-1);
+cell8.innerText = countryinput.value
 
 })
- var tablebody = documen t.getElementById("table-body")
- var asd = tablebody.insertRow(-1);
 
-//  var dur =asd.insertCell(-1);
-//  dur.innerText = finput.value
-//   var dar = asd.insertCell(-1);
-//  dar.innerText = "sinput.value"
-
-//  var cell1 = newRow.insertCell(-1);
-//  cell1.innerText = "finput";
-//  var cell2 = newRow.insertCell(-1);
-//  cell2.innerText = "sinput";
-
-
-//      ${gendername.value}
