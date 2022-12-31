8040a52fef9b94a0ba566f59083895d23f42a24d
NoorulAmeenJ <noorularafat786@gmail.com>
2022-12-31 21:00:24 +0530

    function added

--- script.js
+++ script.js
@@ -10,6 +10,61 @@ var countryinput = document.getElementById("input-country")
 // console.log(gendername)
 var submit =document.getElementById("submit")
 
+var foods = document.getElementById("foods")
+
+ 
+  var fooditems= t("div","id","food-items","FoodItems :")
+ 
+  function t(element,attname,attvalue,innertext){
+    var element = document.createElement(element);
+    element.setAttribute(attname,attvalue);
+    element.innerText= innertext
+   return element    
+}
+  
+  var ul =  li("ul");
+  var li1 = li("li")
+  var li2 = li("li")
+  var li3 = li("li")
+  var li4 = li("li")
+  var li5 = li("li")
+
+  function li(elementname){
+    var element = document.createElement(elementname);
+    return element;
+  }
+
+  var liinuput1 = checkbox("input","type","checkbox","name","food","id","input-Parotta","value","Parotta")
+  var liinuput2 = checkbox("input","type","checkbox","name","food","id","input-Biriyani","value","Biriyani")
+  var liinuput3 = checkbox("input","type","checkbox","name","food","id","input-Dosa","value","Dosa")
+  var liinuput4 = checkbox("input","type","checkbox","name","food","id","input-Idly","value","Idly")
+  var liinuput5 = checkbox("input","type","checkbox","name","food","id","input-Pongal","value","Parotta","Pongal")
+
+  function checkbox(elementname,att1name,att1value,att2name,att2value,att3name,att3value,att4name,att4value){
+          var element = document.createElement(elementname);
+          element.setAttribute(att1name,att1value);
+          element.setAttribute(att2name,att2value);
+          element.setAttribute(att3name,att3value);
+          element.setAttribute(att4name,att4value);
+          return element;
+  }
+
+  var Parotta = t("label","for","inner-Parotta","Parotta" )
+  var Biriyani= t("label","for","inner-Biriyani","Biriyani")
+ var Dosa = t("label","for","inner-Dosa","Dosa" )
+ var Idly = t("label","for","inner-Idly","Idly" )
+ var Pongal= t("label","for","inner-Pongal","Pongal" )
+ 
+foods.append(fooditems)
+foods.append(ul)
+ul.append(li1,li2,li3,li4,li5)
+li1.append(liinuput1,Parotta)
+li2.append(liinuput2,Biriyani)
+li3.append(liinuput3,Dosa)
+li4.append(liinuput4,Idly)
+li5.append(liinuput5,Pongal)
+
+ console.log(document)
 
 submit.addEventListener("click", (e)=>{
     e.preventDefault();
@@ -103,7 +158,7 @@ function reset() {
 reset()
 }
 else{
-    alert(`Please select only 2 food item checkin Gender `)
+    alert(`Please select only 2 food item & checkin Gender `)
 }
 })
 
